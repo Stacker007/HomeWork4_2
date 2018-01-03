19f01a609bf39a64784f26e9e920facb4e31c93f
Stacker007 <34865131+Stacker007@users.noreply.github.com>
2018-01-03 21:13:34 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -14,21 +14,21 @@ int main() {
 
 	int height, width;
 		do {
-		cout << "Введите ширину не менее 2-х" << endl;
+		cout << "Введите ширину не менее 2-х символов " << endl;
 		cin >> width;
 	} while (width < 2);
-	cout << "Введите символ" << endl;
-	cin >> symbol;
+
+
 	do {
-		cout << "Сделайте выбор:\n" << "1-Горизонтальная линия\n" << "2-Вертикальная линия\n";
-		cin >> choice;
-	} while (choice != 1 && choice != 2);
-	switch (choice)
-	{
-	case 1: for (int i = 0; i<count; i++) cout << symbol; break;
-	case 2: for (int i = 0; i<count; i++) cout << symbol << endl; break;
-	}
+		cout << "Введите высоту не менее 2-х символов " << endl;
+		cin >> height;
+	} while (height < 2);
+
+	for (int i = 0; i < width; i++) cout << "*";
 	cout << endl;
+	/////fo
+
+
 	system("pause");
 	return 0;
 
