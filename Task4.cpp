665cb8000b4b6cf51cce834f9311a97c64c01cb4
Stacker007 <34865131+Stacker007@users.noreply.github.com>
2018-01-04 12:29:37 +0200

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -19,8 +19,7 @@ int main()
 		if (width % 2 == 0 && width > 1) cout << "Количество символов автоматически увеличено до " << ++width << endl;
 	} while (width < 1);
 	for (int space = 0, star = width; star > 0; space++, star-=2) {
-		for (int spaceTmp = space; spaceTmp ; spaceTmp--) cout << " ";
-		//cout << space;
+		for (int spaceTmp = space; spaceTmp ; spaceTmp--) cout << " ";  //cout << space;		
 		for (int starTmp = star; starTmp > 0 ; starTmp--) cout << "*";
 		cout << endl;
 	}
