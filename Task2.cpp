bccfaf9687d7c3069512832e4fe90d0f7ff4e9f5
Stacker007 <34865131+Stacker007@users.noreply.github.com>
2018-01-04 07:12:14 +0200

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -23,17 +23,17 @@ int main() {
 		cout << "Введите высоту не менее 2-х символов " << endl;
 		cin >> height;
 	} while (height < 2);
+                         
 
-	for (int i = 0; i < width; i++) cout << "*";
-	cout << endl;
-	for (int k = 1; k < (height - 1); k++) {
-		for (int i = 0; i < width; i++) {
-			if (i == 0 || i == width - 1)  cout << "*";
-			else cout << " ";
-		}
+	for (int k = 0; k < height; k++) {    //Цикл перебора строк
+		if (k == 0 || k == height-1) for (int i = 0; i < width; i++) cout << "*";// Первая и последняя строка из звездочек	
+		else for (int i = 0; i < width; i++) {   //Цикл перебора символов промежуточной строки
+				if (i == 0 || i == width - 1)  cout << "*";// Первый и последний символ промежуточной строки
+				else cout << " "; 	    //Промежуточные символы промежуточной строки (пробелы)
+			}
 		cout << endl;
 	}
-	for (int i = 0; i < width; i++) cout << "*";
+	
 	system("pause");
 	return 0;
 
